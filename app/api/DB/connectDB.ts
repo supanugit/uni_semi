63cb7dc8ca29e7da034d5ae4927898408c9209a2
supanugit <sgitdeploy@gmail.com>
2025-05-20 12:42:39 +0530

    Secound

--- app/api/DB/connectDB.ts
+++ app/api/DB/connectDB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOOSE_URI);
+    await mongoose.connect(process.env.MONGOOSE_URI!);
     console.log("DB Connected...");
   } catch (error) {
     console.log(error instanceof Error ? error.message : error);
