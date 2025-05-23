1e7b680b403fbcd467db2012aa7070e45b1a75d1
supanugit <sgitdeploy@gmail.com>
2025-05-23 08:47:13 +0530

    Error solved

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import { timeTable } from "../DB/model/it_Model";
-import { connectDB } from "../DB/connectDB";
+// import { NextRequest, NextResponse } from "next/server";
+// // import { timeTable } from "../DB/model/it_Model";
+// import { connectDB } from "../DB/connectDB";
 
-export async function PUT(req: NextRequest) {
-  await connectDB();
-  try {
-    const result = await timeTable.updateMany({}, { department: "IT" });
+// export async function PUT(req: NextRequest) {
+//   await connectDB();
+//   try {
+//     // const result = await timeTable.updateMany({}, { department: "IT" });
 
-    return NextResponse.json({
-      status: 200,
-      message: "Department field set to 'IT' for all documents.",
-      updatedCount: result.modifiedCount,
-    });
-  } catch (error) {
-    return NextResponse.json({
-      status: 500,
-      message: "Failed to update documents",
-      error: error instanceof Error ? error.message : error,
-    });
-  }
-}
+//     return NextResponse.json({
+//       status: 200,
+//       message: "Department field set to 'IT' for all documents.",
+//       updatedCount: result.modifiedCount,
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       status: 500,
+//       message: "Failed to update documents",
+//       error: error instanceof Error ? error.message : error,
+//     });
+//   }
+// }
